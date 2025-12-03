c0be3b221d68fe9ccabdc35b1563b201ad788376
Satanasesmipator <luise.garciav@autonoma.edu.co>
2025-12-03 17:16:02 -0500

    Organizando Carpetas

--- genomic_service/apps/genes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import GeneViewSet
-
-router = DefaultRouter()
-router.register(r'', GeneViewSet, basename='gene')
-
-urlpatterns = router.urls
--- genomic_service/apps/patient_reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PatientVariantReportViewSet
-
-router = DefaultRouter()
-router.register(r'', PatientVariantReportViewSet, basename='patient-report')
-
-urlpatterns = router.urls
